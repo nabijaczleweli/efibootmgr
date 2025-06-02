nabijaczleweli/efibootmgr

commit e8ce9fecebd15adb4c60a0678d4c417afe06dde4

Try a little harder on formatting known Loader Optional Data blobs
This tries to do a better job printing optional data blobs:

- if it detects a device path that ends in shim, it assumes it's a file
  path:
  before:
    Boot0000* Linux Firmware Updater	HD(1,GPT,58da233f-b177-4c57-8bee-3d888b3047d4,0x800,0x64000)/File(\EFI\fedora\shimx64.efi)\.f.w.u.p.d.x.6.4...e.f.i...
  after:
    Boot0000* Linux Firmware Updater	HD(1,GPT,58da233f-b177-4c57-8bee-3d888b3047d4,0x800,0x64000)/File(\EFI\fedora\shimx64.efi) File(.\fwupdx64.efi)
- if the loader is 16-bytes, it formats it as an id guid
  before:
    Boot0004* UEFI ATAPI iHAS324 E 3524706 2B8427502710 PciRoot(0x0)/Pci(0x1f,0x2)/Sata(1,65535,0)N.....YM....R,Y.
  after:
    Boot0004* UEFI ATAPI iHAS324 E 3524706 2B8427502710 PciRoot(0x0)/Pci(0x1f,0x2)/Sata(1,65535,0){8108ac4e-9f11-4d59-850e-e21a522c59b2}
  or if it recognizes the guid (which in this case depends on a newer libefivar):
    Boot0004* UEFI ATAPI iHAS324 E 3524706 2B8427502710 PciRoot(0x0)/Pci(0x1f,0x2)/Sata(1,65535,0){auto_created_boot_option}
  where the value between {} is the value from "efivar -L":
    trillian:~/devel/github.com/efivar/master$ LD_LIBRARY_PATH=$PWD/src/ ./src/efivar -L | grep auto_created_boot_option
    {8108ac4e-9f11-4d59-850e-e21a522c59b2} {auto_created_boot_option} efi_guid_auto_created_boot_option	Automatically Created Boot Entry

Signed-off-by: Peter Jones <pjones@redhat.com>

src/efibootmgr.c
@@ -37,6 +37,7 @@
 #include <err.h>
 #include <errno.h>
 #include <limits.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -52,7 +53,7 @@
 
 #include "list.h"
 #include "efi.h"
-#include "unparse_path.h"
+#include "parse_loader_data.h"
 #include "efibootmgr.h"
 #include "error.h"
 
@@ -926,71 +927,117 @@ show_var_path(efi_load_option *load_option, size_t boot_data_size)
 	size_t text_path_len = 0;
 	uint16_t pathlen;
 	ssize_t rc;
-
 	efidp dp = NULL;
 	unsigned char *optional_data = NULL;
 	size_t optional_data_len=0;
+	bool is_shim = false;
+	const char * const shim_path_segments[] = {
+		"/File(\\EFI\\", "\\shim", ".efi)", NULL
+	};
 
 	pathlen = efi_loadopt_pathlen(load_option,
 				      boot_data_size);
 	dp = efi_loadopt_path(load_option, boot_data_size);
-	rc = efidp_format_device_path(text_path, text_path_len,
-				      dp, pathlen);
+	rc = efidp_format_device_path((unsigned char *)text_path,
+				      text_path_len, dp, pathlen);
 	if (rc < 0) {
 		warning("Could not parse device path");
-                return;
-        }
+		return;
+	}
 	rc += 1;
 
 	text_path_len = rc;
 	text_path = calloc(1, rc);
 	if (!text_path) {
 		warning("Could not parse device path");
-                return;
-        }
+		return;
+	}
 
-	rc = efidp_format_device_path(text_path, text_path_len,
-				      dp, pathlen);
-        if (rc >= 0)
-	        printf("\t%s", text_path);
+	rc = efidp_format_device_path((unsigned char *)text_path,
+				      text_path_len, dp, pathlen);
+	if (rc >= 0) {
+		printf("\t%s", text_path);
+
+		char *a = text_path;
+		for (int i = 0; a && shim_path_segments[i] != NULL; i++) {
+			a = strstr(a, shim_path_segments[i]);
+			if (a)
+				a += strlen(shim_path_segments[i]);
+		}
+		if (a && a[0] == '\0')
+			is_shim = true;
+	}
+
 	free(text_path);
 	if (rc < 0) {
 		warning("Could not parse device path");
-                return;
-        }
+		return;
+	}
 
 	/* Print optional data */
 	rc = efi_loadopt_optional_data(load_option, boot_data_size,
 				       &optional_data, &optional_data_len);
 	if (rc < 0) {
 		warning("Could not parse optional data");
-                return;
-        }
+		return;
+	}
 
-	if (opts.unicode) {
+	typedef ssize_t (*parser_t)(char *buffer, size_t buffer_size,
+				    uint8_t *p, uint64_t length);
+	parser_t parser = NULL;
+	if (is_shim && optional_data_len) {
+		char *a = ucs2_to_utf8((uint16_t*)optional_data,
+				       optional_data_len/2);
+		if (!a) {
+			warning("Could not parse optional data");
+			return;
+		}
+		text_path = calloc(1, sizeof(" File(.")
+				      + strlen(a)
+				      + strlen(")"));
+		if (!text_path) {
+			warning("Could not parse optional data");
+			return;
+		}
+		char *b;
+
+		b = stpcpy(text_path, " File(.");
+		b = stpcpy(b, a);
+		b = stpcpy(b, ")");
+		free(a);
+	} else if (opts.unicode) {
 		text_path = ucs2_to_utf8((uint16_t*)optional_data,
 					 optional_data_len/2);
+		if (!text_path) {
+			warning("Could not parse optional data");
+			return;
+		}
+	} else if (optional_data_len == sizeof(efi_guid_t)) {
+		parser = parse_efi_guid;
 	} else {
-		rc = unparse_raw_text(NULL, 0, optional_data,
-				      optional_data_len);
+		parser = parse_raw_text;
+	}
+
+	if (parser) {
+		rc = parser(NULL, 0, optional_data, optional_data_len);
 		if (rc < 0) {
 			warning("Could not parse optional data");
-                        return;
-                }
+			return;
+		}
 		rc += 1;
 		text_path_len = rc;
 		text_path = calloc(1, rc);
 		if (!text_path) {
 			warning("Could not parse optional data");
-                        return;
-                }
-		rc = unparse_raw_text(text_path, text_path_len,
-				      optional_data, optional_data_len);
+			return;
+		}
+		rc = parser(text_path, text_path_len,
+			    optional_data, optional_data_len);
 		if (rc < 0) {
 			warning("Could not parse device path");
-                        free(text_path);
-                        return;
-                }
+			free(text_path);
+			return;
+		}
 	}
 	printf("%s", text_path);
 	free(text_path);

src/Makefile
@@ -12,9 +12,9 @@ TARGETS=$(BINTARGETS) efibootmgr.8 efibootdump.8
 
 all : deps $(TARGETS)
 
-EFIBOOTMGR_SOURCES = efibootmgr.c efi.c unparse_path.c
+EFIBOOTMGR_SOURCES = efibootmgr.c efi.c parse_loader_data.c
 EFICONMAN_SOURCES = eficonman.c
-EFIBOOTDUMP_SOURCES = efibootdump.c unparse_path.c
+EFIBOOTDUMP_SOURCES = efibootdump.c parse_loader_data.c
 EFIBOOTNEXT_SOURCES = efibootnext.c
 ALL_SOURCES=$(EFIBOOTMGR_SOURCES)
 -include $(call deps-of,$(ALL_SOURCES))

src/efibootdump.c
@@ -24,7 +24,7 @@
 #include <unistd.h>
 
 #include "error.h"
-#include "unparse_path.h"
+#include "parse_loader_data.h"
 
 int verbose;
 
@@ -68,8 +68,8 @@ print_boot_entry(efi_load_option *loadopt, size_t data_size)
 	text_path = alloca(text_path_len);
 	if (!text_path)
 		error(100, "Couldn't allocate memory");
-	rc = efidp_format_device_path(text_path, text_path_len,
-				      dp, pathlen);
+	rc = efidp_format_device_path((unsigned char *)text_path,
+				      text_path_len, dp, pathlen);
 	if (rc < 0) {
 		printf("<bad device path>");
 		return;
@@ -84,7 +84,7 @@ print_boot_entry(efi_load_option *loadopt, size_t data_size)
 		return;
 	}
 
-	rc = unparse_raw_text(NULL, 0, optional_data, optional_data_len);
+	rc = parse_raw_text(NULL, 0, optional_data, optional_data_len);
 	if (rc < 0) {
 		printf("<bad optional data>");
 		return;
@@ -95,7 +95,7 @@ print_boot_entry(efi_load_option *loadopt, size_t data_size)
 	if (!raw)
 		error(101, "Couldn't allocate memory");
 
-	rc = unparse_raw_text(raw, raw_len, optional_data, optional_data_len);
+	rc = parse_raw_text(raw, raw_len, optional_data, optional_data_len);
 	if (rc < 0) {
 		printf("<bad optional data>");
 	} else if (rc > 0) {

src/eficonman.c
@@ -77,7 +77,7 @@ do_list(void)
 		dp = whole_dp;
 		while (dp) {
 			ssize_t sz, ssz;
-			char *s = NULL;
+			unsigned char *s = NULL;
 
 			if (efidp_is_multiinstance(dp)) {
 				sz = efidp_instance_size(dp);

src/include/parse_loader_data.h
@@ -1,8 +1,7 @@
 /*
-  unparse_path.[ch]
- 
-  Copyright (C) 2001 Dell Computer Corporation <Matt_Domsch@dell.com>
- 
+  Copyright 2001 Dell Computer Corporation <Matt_Domsch@dell.com>
+  Copyright 2014-2019 Peter Jones <pjones@redhat.com>
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
@@ -18,13 +17,15 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef _UNPARSE_PATH_H
-#define _UNPARSE_PATH_H
+#ifndef _PARSE_LOADER_DATA_H
+#define _PARSE_LOADER_DATA_H
 
 #include <stdint.h>
 #include "efi.h"
 
-ssize_t unparse_raw_text(char *buffer, size_t buffer_size,
-			uint8_t *p, uint64_t length);
+ssize_t parse_efi_guid(char *buffer, size_t buffer_size,
+		       uint8_t *p, uint64_t length);
+ssize_t parse_raw_text(char *buffer, size_t buffer_size,
+		       uint8_t *p, uint64_t length);
 
 #endif

src/parse_loader_data.c
@@ -32,13 +32,29 @@
 #include <netinet/in.h>
 
 #include "efi.h"
-#include "unparse_path.h"
+#include "parse_loader_data.h"
 
 /* Avoid unaligned access warnings */
 #define get(buf, obj) *(typeof(obj) *)memcpy(buf, &obj, sizeof(obj))
 
+extern int verbose;
+
 ssize_t
-unparse_raw_text(char *buffer, size_t buffer_size, uint8_t *p, uint64_t length)
+parse_efi_guid(char *buffer, size_t buffer_size, uint8_t *p, uint64_t length)
+{
+	ssize_t needed = 0;
+
+	if (length == sizeof(efi_guid_t)) {
+		needed = efi_guid_to_id_guid((efi_guid_t *)p, NULL);
+		if (buffer && needed > 0 && buffer_size >= (size_t)needed)
+			needed = efi_guid_to_id_guid((efi_guid_t *)p, &buffer);
+	}
+
+	return needed;
+}
+
+ssize_t
+parse_raw_text(char *buffer, size_t buffer_size, uint8_t *p, uint64_t length)
 {
 	uint64_t i; unsigned char c;
 

commit a6f55ff349fe4a06aef73865f35bc226df0faa81

Format "File(\" instead of just "File(...shim....efi)" as string
Before:

nabijaczleweli@chrust:/tmp/efibootmgr/efibootmgr$ efibootmgr
BootCurrent: 0001
Timeout: 2 seconds
BootOrder: 0001,0004,0002,0003,0007,0000
Boot0000* Enter Setup   FvVol(8063c21a-8e58-4576-95ce-089e87975d23)/FvFile(462caa21-7614-4503-836e-8ab6f4662331)
Boot0001* Debian GNU/Linux trixie/sid with Linux 6.6.9-amd64    HD(1,GPT,b56e9130-b36e-4b7b-9abf-6fe3cb9606c7,0x800,0x79800)/File(\KLAPKI\DD956699294C45E99FFE000DF2D986D6\6.6.9-AMD64\VMLINUZ-6.6.9-AMD64)69006e0069007400720064003d005c006b006c00610070006b0069005c00640064003900350036003600390039003200390034006300340035006500390039006600660065003000300030006400660032006400390038003600640036005c0036002e0036002e0039002d0061006d006400360034005c0069006e0069007400720064002e0069006d0067002d0036002e0036002e0039002d0061006d00640036003400200072006f006f0074003d007a00660073003a004100550054004f00200072006500730075006d0065003d0050004100520054004c004100420045004c003d006300680072007500730074002d007300770061007000200069006e00740065006c005f0069006f006d006d0075003d006f006e002000730079007300630061006c006c002e007800330032003d007900200071007500690065007400
Boot0002* eMMC Device   PciRoot(0x0)/Pci(0x1c,0x0)/eMMC(0)/Ctrl(0x0){auto_created_boot_option}
Boot0003* UEFI Shell    FvVol(8063c21a-8e58-4576-95ce-089e87975d23)/FvFile(7c04a583-9e3e-4f1c-ad65-e05268d0b4d1)
Boot0004* Debian GNU/Linux trixie/sid with Linux 6.6.9-amd64 (verbose debug)    HD(1,GPT,b56e9130-b36e-4b7b-9abf-6fe3cb9606c7,0x800,0x79800)/File(\CHRUST\6.6.9-AMD64\VMLINUZ-6.6.9-AMD64)69006e0069007400720064003d005c006b006c00610070006b0069005c00640064003900350036003600390039003200390034006300340035006500390039006600660065003000300030006400660032006400390038003600640036005c0036002e0036002e0039002d0061006d006400360034005c0069006e0069007400720064002e0069006d0067002d0036002e0036002e0039002d0061006d00640036003400200072006f006f0074003d007a00660073003a004100550054004f00200072006500730075006d0065003d0050004100520054004c004100420045004c003d006300680072007500730074002d007300770061007000200069006e00740065006c005f0069006f006d006d0075003d006f006e002000730079007300630061006c006c002e007800330032003d007900200076006500720062006f0073006500200064006500620075006700
Boot0007* debian        HD(2,GPT,4ad22847-4499-314f-8d8f-866509d94fa6,0x1000,0xff800)/File(\EFI\debian\shimx64.efi)
nabijaczleweli@chrust:/tmp/efibootmgr/efibootmgr$ efibootmgr  -u
BootCurrent: 0001
Timeout: 2 seconds
BootOrder: 0001,0004,0002,0003,0007,0000
Boot0000* Enter Setup   FvVol(8063c21a-8e58-4576-95ce-089e87975d23)/FvFile(462caa21-7614-4503-836e-8ab6f4662331)
Boot0001* Debian GNU/Linux trixie/sid with Linux 6.6.9-amd64    HD(1,GPT,b56e9130-b36e-4b7b-9abf-6fe3cb9606c7,0x800,0x79800)/File(\KLAPKI\DD956699294C45E99FFE000DF2D986D6\6.6.9-AMD64\VMLINUZ-6.6.9-AMD64)initrd=\klapki\dd956699294c45e99ffe000df2d986d6\6.6.9-amd64\initrd.img-6.6.9-amd64 root=zfs:AUTO resume=PARTLABEL=chrust-swap intel_iommu=on syscall.x32=y quiet
Boot0002* eMMC Device   PciRoot(0x0)/Pci(0x1c,0x0)/eMMC(0)/Ctrl(0x0)걎脈鼑䵙຅᫢ⱒ뉙
Boot0003* UEFI Shell    FvVol(8063c21a-8e58-4576-95ce-089e87975d23)/FvFile(7c04a583-9e3e-4f1c-ad65-e05268d0b4d1)
Boot0004* Debian GNU/Linux trixie/sid with Linux 6.6.9-amd64 (verbose debug)    HD(1,GPT,b56e9130-b36e-4b7b-9abf-6fe3cb9606c7,0x800,0x79800)/File(\CHRUST\6.6.9-AMD64\VMLINUZ-6.6.9-AMD64)initrd=\klapki\dd956699294c45e99ffe000df2d986d6\6.6.9-amd64\initrd.img-6.6.9-amd64 root=zfs:AUTO resume=PARTLABEL=chrust-swap intel_iommu=on syscall.x32=y verbose debug
Boot0007* debian        HD(2,GPT,4ad22847-4499-314f-8d8f-866509d94fa6,0x1000,0xff800)/File(\EFI\debian\shimx64.efi)

After:

$ ./src/efibootmgr
BootCurrent: 0001
Timeout: 2 seconds
BootOrder: 0001,0004,0002,0003,0007,0000
Boot0000* Enter Setup   FvVol(8063c21a-8e58-4576-95ce-089e87975d23)/FvFile(462caa21-7614-4503-836e-8ab6f4662331)
Boot0001* Debian GNU/Linux trixie/sid with Linux 6.6.9-amd64    HD(1,GPT,b56e9130-b36e-4b7b-9abf-6fe3cb9606c7,0x800,0x79800)/File(\KLAPKI\DD956699294C45E99FFE000DF2D986D6\6.6.9-AMD64\VMLINUZ-6.6.9-AMD64) File(.initrd=\klapki\dd956699294c45e99ffe000df2d986d6\6.6.9-amd64\initrd.img-6.6.9-amd64 root=zfs:AUTO resume=PARTLABEL=chrust-swap intel_iommu=on syscall.x32=y quiet)
Boot0002* eMMC Device   PciRoot(0x0)/Pci(0x1c,0x0)/eMMC(0)/Ctrl(0x0){auto_created_boot_option}
Boot0003* UEFI Shell    FvVol(8063c21a-8e58-4576-95ce-089e87975d23)/FvFile(7c04a583-9e3e-4f1c-ad65-e05268d0b4d1)
Boot0004* Debian GNU/Linux trixie/sid with Linux 6.6.9-amd64 (verbose debug)    HD(1,GPT,b56e9130-b36e-4b7b-9abf-6fe3cb9606c7,0x800,0x79800)/File(\CHRUST\6.6.9-AMD64\VMLINUZ-6.6.9-AMD64) File(.initrd=\klapki\dd956699294c45e99ffe000df2d986d6\6.6.9-amd64\initrd.img-6.6.9-amd64 root=zfs:AUTO resume=PARTLABEL=chrust-swap intel_iommu=on syscall.x32=y verbose debug)
Boot0007* debian        HD(2,GPT,4ad22847-4499-314f-8d8f-866509d94fa6,0x1000,0xff800)/File(\EFI\debian\shimx64.efi)

Closes: #195
Closes: https://bugs.debian.org/1055526
Fixes: e8ce9fe ("Try a little harder on formatting known

src/efibootmgr.c
@@ -974,9 +974,6 @@ show_var_path(efi_load_option *load_option, size_t boot_data_size)
 	unsigned char *optional_data = NULL;
 	size_t optional_data_len=0;
 	bool is_shim = false;
-	const char * const shim_path_segments[] = {
-		"/File(\\EFI\\", "\\shim", ".efi)", NULL
-	};
 
 	pathlen = efi_loadopt_pathlen(load_option,
 				      boot_data_size);
@@ -1001,13 +998,7 @@ show_var_path(efi_load_option *load_option, size_t boot_data_size)
 	if (rc >= 0) {
 		printf("\t%s", text_path);
 
-		char *a = text_path;
-		for (int i = 0; a && shim_path_segments[i] != NULL; i++) {
-			a = strstr(a, shim_path_segments[i]);
-			if (a)
-				a += strlen(shim_path_segments[i]);
-		}
-		if (a && a[0] == '\0')
+		if (strstr(text_path, "/File(\\"))
 			is_shim = true;
 	}
 